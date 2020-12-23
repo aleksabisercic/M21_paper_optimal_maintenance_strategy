7a3daf44dccfaaeca869c86548591faa980a4eb6
aleksabisercic <64646644+aleksabisercic@users.noreply.github.com>
2020-12-23 14:43:25 +0100

    Results_per_window_step

---
+++ Machine_learning_simulations/4.Statistic Sim/1.Probibalistic/1. Test_Sim/Gen_window.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  6 13:55:33 2020
+
+@author: Freedom
+"""
+import numpy as np 
+import matplotlib.pyplot as plt
+
+#Time of simulation
+vreme_simulacije = 259200 # len of  test ( 6 month period )
+
+vremena_otkaza = np.load('lista_vremena_otkz_1000_BTD.npy', allow_pickle=True)
+vremena_popravke = np.load('lista_vremena_pop_1000_BTD.npy', allow_pickle=True)
+vrsta_otkaza = np.load('lista_vrsta_pop_1000_BTD.npy', allow_pickle=True)
+
+for i in range(2):
+    ls_ = []
+    podatci1 = vremena_otkaza[i].reshape(-1)
+    podatci2 = vremena_popravke[i].reshape(-1)
+    podatci3 = vrsta_otkaza[i].reshape(-1)
+
+    #drop if the last value is larger then len_of_simulation
+    if vreme_simulacije < podatci1[-1]:
+        podatci1[:-1]
+    if vreme_simulacije < podatci2[-1]:
+        podatci2[:-1]
+
+    #make list vector with cosistant(same) len
+    #sometimes one vector is longer than another for single value
+    len_1 = len(podatci1)
+    len_2 = len(podatci2)
+    len_3 = len(podatci3)
+    ls_.extend((len_1, len_2, len_3))
+    min_len = min(ls_)
+    podatci1 = podatci1[:min_len]
+    podatci2 = podatci2[:min_len]
+    podatci3 = podatci3[:min_len]
+
+    def gen_lambda_and_mi(podatci1,podatci2, podatci3, seq_len, t):
+        '''
+        This Funcion consist of 3 parts:
+        1. We generate 3 matrix with len(len_of_simulation(in minutes))
+        2. We fill in which moment (minut) the event happend
+        3. We go through Matrixes with sliding_window (window size, and step) 
+        '''
+
+        #1. step: Generate matrix for each list
+        matrix = np.zeros(vreme_simulacije)
+        matrix1 = np.zeros(vreme_simulacije)
+        matrix2 = np.zeros(vreme_simulacije)
+
+        #2. step: Fill the each Matrix with mooments 
+        #(in which minute) event happend (or what type of event happend in that moment (matrix2))
+        for index, value in enumerate(podatci1):
+            matrix[int(value)] = 1
+            matrix2[int(value)] = podatci3[index] + 1      
+        for i in podatci2:
+            matrix1[int(i)] = 1 
+
+        #3. step: Evaluate for window_size(seq_len) and step(dt)         
+        lambd = []
+        mi = [] 
+        fail_distribution = []   
+        start = 0
+        end = seq_len
+        one_day = (24*60) / seq_len #when multiplied with window_size(seq_len) returns values scaled to 1 day
+        for i in range(int((len(matrix)-seq_len)/t)):
+            #Generate fail number per window and step
+            ls = matrix[start:end]
+            lambd.append(sum(ls)*one_day) #Number of Failures per 1 day for current window and step
+            #Generate repair number per window and step
+            ls1 = matrix1[start:end]
+            mi.append(sum(ls1)*one_day) #Number of Repairs per 1 day for current window and step
+            #Generate fail_distribution per window and step
+            types_of_fail_ = []
+            types_of_fail_.extend(matrix2[start:end])
+            Mechanical_fail = types_of_fail_.count(1)
+            Electro_fail = types_of_fail_.count(2) 
+            Other_fail = types_of_fail_.count(3) 
+            total_fail = Mechanical_fail + Electro_fail + Other_fail
+            ls_ = [Mechanical_fail/total_fail, Electro_fail/total_fail, Other_fail/total_fail]
+            fail_distribution.append(ls_) 
+             
+            start += t #update for step(dt)
+            end += t #update for step(dt)
+        return lambd, mi, fail_distribution
+            
+    seq_leng = [7*24*60, 15*24*60, 30*24*60] #windows of 7, 15 and 30 days
+    dt = [60] #step of 60 minutes
+
+    for seq_len in seq_leng:
+        for t in dt:
+            lamb, mi, fail_distribution =  gen_lambda_and_mi(podatci1,podatci2,podatci3, int(seq_len), t)
+            mi_gen_simulacija = np.array(mi).reshape(-1, 1)
+            lamb_gen = np.array(lamb).reshape(-1, 1)
+            fail_distribution = np.asarray(fail_distribution)
+            print(fail_distribution.shape)
+            np.save('fail_window{}_dt{}_sim{}.npy'.format(seq_len/(24*60), t, i), lamb_gen)
+            np.save('repair_window{}_dt{}_sim{}.npy'.format(seq_len/(24*60), t, i), lamb_gen)
+            np.save('class_window{}_dt{}_sim{}.npy'.format(seq_len/(24*60), t, i), lamb_gen)
+
+
+
+
+
+
+
+# sim_name_lam = 'Failure_rates_' + str(t) + 'dt_' + str(seq_len) + 'min_simulacija' + '.npy'
+# sim_name_mi = 'Repair_rates' + str(t) + 'dt' + str(seq_len) + 'min_simulacija' + '.npy'
+# np.save(sim_name_lam, lamb_gen_simulacija)
+# np.save(sim_name_mi +str(t), mi_gen_simulacija)
+    
